29afca092e22d7920ea2369360d9d38eab678ee7
LiangKai Zhang <zhangliangkai.wh@gmail.com>
2020-09-10 01:53:02 +0800

    org-babel

--- init.el
+++ init.el
@@ -311,6 +311,19 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (eval-after-load 'org
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((C . t)
+       (emacs-lisp . t)
+       )
+     )
+    )
+  (message "*************************************************************************")
+  (message "*************************************************************************")
+  (message "******************************zlk-user-init******************************")
+  (message "*************************************************************************")
+  (message "*************************************************************************")
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
